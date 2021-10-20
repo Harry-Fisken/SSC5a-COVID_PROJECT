02bd7f96b6e70162dd905b8719ceda87f4720df7
Harry-Fisken <89106502+Harry-Fisken@users.noreply.github.com>
2021-10-20 20:22:02 +0100

    Update SSC5a Covid19 Project .R

--- SSC5a Covid19 Project .R
+++ SSC5a Covid19 Project .R
@@ -9,12 +9,15 @@ covid_global <- read_csv('https://covid.ourworldindata.org/data/owid-covid-data.
 covid_UK <- covid_global %>% filter(iso_code == 'GBR')
 
 
+Colnames.Covid <- colnames(covid_UK, do.NULL = TRUE, prefix = "col")
+View(Colnames.Covid)
+
 #Total Number of Deaths 
 covid_UK %>%
   ggplot(aes(x = date, y = total_deaths)) + geom_line() +
   scale_x_date(date_breaks = "1 month", date_labels = '%D')
 
-#New Deaths
+#New Deaths#
 covid_global %>%
   filter(iso_code == 'GBR') %>%
   ggplot(aes(x = date, y = new_deaths)) + geom_line() +
@@ -27,7 +30,7 @@ covid_global %>%
   ggplot(aes(x = date, y = )) + geom_line() +
   scale_x_date(date_breaks = "", date_labels = '%D')
 
-#Daily COVID-19 Tests 
+#Daily COVID-19 Tests# 
 covid_UK %>%
   ggplot(aes(x = date, y = new_tests)) + geom_line() + 
   scale_x_date(date_breaks = "1 month", date_labels = '%D') +
@@ -35,7 +38,7 @@ covid_UK %>%
   labs(x = "Date", y = 'New COVID-19 Tests (n)', title = 'Number of Daily COVID-19 Tests in the UK from March 2020 - August 2021')
 
 
-#Number of Hopsital Patients 
+#Number of Hopsital Patients# 
 covid_UK %>%
   
   covid_UK %>% filter(!is.na(weekly_hosp_admissions)) %>% 
@@ -45,7 +48,7 @@ covid_UK %>%
   scale_y_continuous(labels = scales::comma) +
   labs(x = "Date", y = 'Hospital Patients (n)', title = 'Number of COVID-19 Hospital Patients in the UK Between March 2020 - August 2021')
 
-#Number of Hopsital Patients 
+#Number of Hopsital Patients# 
 covid_global %>%
   filter(iso_code == 'GBR') %>%
   ggplot(aes(x = date, y = icu_patients)) + geom_line() +
@@ -53,7 +56,7 @@ covid_global %>%
   scale_y_continuous(labels = scales::comma) +
   labs(x = "Date", y = 'ICU Patients (n)', title = 'Number of COVID-19 ICU Patients in the UK Between March 2020 - August 2021')
 
-##Weekly Hospital Admissions###
+###Weekly Hospital Admissions###
 
 covid_UK %>% filter(!is.na(weekly_hosp_admissions)) %>% 
   
@@ -66,15 +69,35 @@ covid_UK %>% filter(!is.na(weekly_hosp_admissions)) %>%
 
 
 
-
-
-
-
 ##Regression Discontinuity##
+  
+options(date.origin = "1970-01-01")
+as.date <- function(x, origin = getOption("date.origin"))
 
+start_time <- 2020-06-19
+start_date <- as.Date(start_time, format = "%y-%m-%d")
+
+change_time <- 2020-07-24
+change_date <- as.Date(change_time, format = "%d-%m-%y")
+  
+end_time <- 2020-08-23
+end_date <- as.Date(end_time, format = "%d-%m-%y")
+
+
+covid_UK <- filter(covid_UK, date >= start_date & date <= end_date)
+
+covid_UK <- mutate(covid_UK, 
+                   day = as.numeric(date - start_date),
+                   change = ifelse(date > change_date, 1, 0),
+                   days_postchange = day * change)
+
+Colnames.Covid2 <- colnames(covid_UK, do.NULL = TRUE, prefix = "col")
+View(Colnames.Covid2)
+view(covid_UK)
 #Model 0
 
-covid_UK %>%
-  model0 <-glm(new-hosp_admissions ~ 2020-06-19 + 2020-07-24 + 2020-08-28, family="poisson", data=covid_UK, subset=year01==0)
+model0 <-glm(new_deaths ~ day+change+days_postchange, family="poisson", data = covid_UK)
+
+
 
 
